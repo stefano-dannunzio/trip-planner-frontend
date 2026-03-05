18b8cabd8de4b13cb800991c8500735cf0474fb1
Stefano D'Annunzio <89990955+stefano-dannunzio@users.noreply.github.com>
2026-03-05 14:08:53 -0300

    Ready for deploy

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. Create a custom Axios instance
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // 2. REQUEST INTERCEPTOR: Automatically attach the token to every outgoing request
